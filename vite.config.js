f9e4ac2e17c2fc5abcba87a0346ca69f5aed596c
Gowthampoo <tmkgangstar@gmail.com>
2026-07-06 20:15:25 +0530

    Add GH Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/maa-mantra/',
-  plugins: [react()],
-  // rest stays same
+  base: '/maa-mantra-ventures/',
+  // ...rest
 })
